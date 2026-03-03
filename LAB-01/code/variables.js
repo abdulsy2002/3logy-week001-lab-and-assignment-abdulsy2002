7ab1693a4e2679f068492509f44057ed175b09e6
abdurrahman <abdurrahmansuleimangusau@gmail.com>
2026-03-04 00:06:02 +0100

    Added variables.js for LAB-01

---
+++ LAB-01/code/variables.js
@@ -0,0 +1,12 @@
+const myName = "Abdul"; 
+let myAge = 23;
+let isStudent = true; 
+let favProgLang = "Html"; 
+
+console.log("Name:", myName);
+console.log("Age:", myAge);
+console.log("Is Student:", isStudent);
+console.log("Favorite Programming Language:", favProgLang);
+
+
+
