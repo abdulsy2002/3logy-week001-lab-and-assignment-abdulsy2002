64527aa590a15b9626685225cb9226f49ff1550f
abdurrahman <abdurrahmansuleimangusau@gmail.com>
2026-03-06 18:06:06 +0100

    TICKET-01

---
+++ TICKET-01/code/calculator.js
@@ -0,0 +1,11 @@
+let num1 =5;
+let num2 =6;
+let sum=num1+num2;
+let difference=num1-num2;
+let product=num1*num2;
+let quotient=num1/num2;
+console.log("The sum of", num1, "and", num2, "is:", sum);
+console.log("The difference of", num1, "and", num2, "is:", difference);
+console.log("The product of", num1, "and", num2, "is:", product);
+console.log("The quotient of", num1, "and", num2, "is:", quotient);
+
